a53d7cfe0f944bbd96a1be8948a3572b2c37cf09
Nahir Martinez <nahirmartinez09@gmail.com>
2023-06-02 12:31:37 -0300

    cuarto commit

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const ulInformacion = document.querySelector ("#info-reserva");
-const selectCantCamas = document.querySelector ("#cant-camas");
-const selectCantDias = document.querySelector ("#cant-dias");
-const selectDiaIngreso = document.querySelector ("#dia-ingreso");
-const selectServicios = document.querySelector ("#servicios-adicionales");
+const ulInformacionReserva = document.querySelector ("ul");
+const selectOpcionReserva = document.querySelector ("select");
+const divOpcionesReserva = document.querySelector ("#opciones-reserva");
+const spanTituloOpcion = document.querySelector ("#opciones-reserva span");
 
-const pCamasTotal = document.querySelector ("#camas-total");
-const pDiasTotal = document.querySelector ("#dias-total");
-const pDiaIngreso = document.querySelector ("#Ingreso-reserva");
-const pServiciosAdicionales = document.querySelector ("#servicio-comida");
+const OPC_1_CANT_DIAS = 1;
+const OPC_2_CANT_DIAS = 3;
+const OPC_3_CANT_DIAS = 7;
 
-let cantCamas = 0;
-let cantDias = 0;
-let diaDeIngreso = 0;
-let serviciosComida = 0;
+const OPC_1_DIA_INGRESO = "Lunes";
+const OPC_2_DIA_INGRESO = "Miercoles";
+const OPC_3_DIA_INGRESO = "Sabado";
+
+const OPC_1_SERV_EXTRA = "Desayuno";
+const OPC_2_SERV_EXTRA = "Desayuno y almuerzo";
+const OPC_3_SERV_EXTRA = "Desayuno, almuerzo y cena";
+
+const OPC_1 = 1;
+const OPC_2 = 2;
+const OPC_3 = 3;
+const OPC_4 = 4;
+
+let opcActual = OPC_1;
 
 function agregarInformacion(){
 
-    cantCamas = (selectCantCamas.value);
+    cantCamas = (selectOpcionReserva.value);
     cantDias = (selectCantDias.value);
     diaDeIngreso = (selectDiaIngreso.value);
     serviciosComida = selectServicios.value;
@@ -26,5 +34,66 @@ function agregarInformacion(){
     pDiaIngreso.innerHTML = `El dia de ingreso es ${diaDeIngreso}`;
     pServiciosAdicionales.innerHTML = `Los servicios adicionales seleccionados son: ${serviciosComida}`;
 
-    ulInformacion.innerHTML = `<li>Las cantidad de camas son ${cantCamas} <br> La cantidad de dias son ${cantDias} <br> El dia de ingreso es ${diaDeIngreso} <br> Los servicios adicionales seleccionados son: ${serviciosComida}</li>`;
+    ulInformacionReserva.innerHTML = `<li>Las cantidad de camas son ${cantCamas} <br> La cantidad de dias son ${cantDias} <br> El dia de ingreso es ${diaDeIngreso} <br> Los servicios adicionales seleccionados son: ${serviciosComida}</li>`;
+}
+
+function actualizaOpcReserva() {
+    switch (opcActual) {
+        case OPC_1:
+            actualizaAOpcionInfo2();
+            break;
+        case OPC_2:
+            actualizaAOpcionInfo3();
+            break;
+        case OPC_3:
+            actualizaAOpcionInfo4();
+            break;
+        case OPC_4:
+            actualizaOpcionesATerminado();
+    }
+    opcActual++;
+}
+
+function actualizaOpcionesATerminado(){
+    divOpcionesReserva.innerHTML = `
+        <h2>Reserva finalizada</h2>
+    `;
+}
+
+function actualizaAOpcionInfo4(){
+    spanTituloOpcion.innerHTML = "Seleccione servicio extra:";
+    selectOpcionReserva.innerHTML = `
+        <option value="${OPC_1_SERV_EXTRA}">${OPC_1_SERV_EXTRA}</option>
+        <option value="${OPC_2_SERV_EXTRA}">${OPC_2_SERV_EXTRA}</option>
+        <option value="${OPC_3_SERV_EXTRA}">${OPC_3_SERV_EXTRA}</option>
+    `;
+}
+
+function actualizaAOpcionInfo3() {
+    spanTituloOpcion.innerHTML = "Seleccione día de ingreso:";
+    selectOpcionReserva.innerHTML = `
+        <option value="${OPC_1_DIA_INGRESO}">${OPC_1_DIA_INGRESO}</option>
+        <option value="${OPC_2_DIA_INGRESO}">${OPC_2_DIA_INGRESO}</option>
+        <option value="${OPC_3_DIA_INGRESO}">${OPC_3_DIA_INGRESO}</option>
+    `;
+}
+
+function actualizaAOpcionInfo2() {
+    spanTituloOpcion.innerHTML = "Seleccione cantidad de días";
+    selectOpcionReserva.innerHTML = `
+                <option value="${OPC_1_CANT_DIAS} día">${OPC_1_CANT_DIAS} día</option>
+                <option value="${OPC_2_CANT_DIAS} días">${OPC_2_CANT_DIAS} días</option>
+                <option value="${OPC_3_CANT_DIAS} días">${OPC_3_CANT_DIAS} días</option>
+            `;
+}
+
+function actualizarOpcSeleccionadas() {
+    ulInformacionReserva.innerHTML += `
+        <li>${selectOpcionReserva.value}</li>
+    `;
+}
+
+ function enviarInformacion(){
+    actualizarOpcSeleccionadas();
+    actualizaOpcReserva();
 }
